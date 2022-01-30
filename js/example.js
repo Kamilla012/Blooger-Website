0a623cd70d280b5bdb0c41c7c8d0e31e6561d26b
Kamilla <kamilladrozdz0@gmail.com>
2022-01-30 10:49:21 +0100

    java script
    dots and arrow right

---
+++ js/example.js
@@ -0,0 +1,32 @@
+/*
+let myFirstFunction = (name, greeting) => {
+    console.log(greeting + '! mam na imię' + ' ' + name);
+};
+myFirstFunction('Kacper', 'Hej');
+myFirstFunction('Jan', 'Witam');
+*/
+
+/* Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dwa
+ i wypisze wynik na konsolę. */
+
+ let mathematicalFunction1 = (number) => {
+     console.log(number*2);
+ };
+
+ mathematicalFunction1(5);
+
+ /* Napisz funkcję, która przemnoży liczbę przekazaną w
+  parametrze przez dowolną liczbę, przekazaną w kolejnym parametrze.*/
+
+let mathematicalFunction2 = (number1, number2) => {
+    console.log(number1*number2);
+};
+
+mathematicalFunction2(2, 3);
+
+/*Napisz funkcję, która podniesie liczbę przekazaną w parametrze do potęgi drugiej*/
+let mathematicalFunction3 = (number3) => {
+    console.log(number3*number3)
+};
+
+mathematicalFunction3(8);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let activeSlideNumber = 1;
+
 let arrowLeft = document.querySelector('.arrow-left');
 
 let arrowRight = document.querySelector('.arrow-right');
@@ -10,5 +12,38 @@ let dot3 = document.querySelector('#dot3');
 let slide1 = document.querySelector('#slide1');
 let slide2 = document.querySelector('#slide2');
 let slide3 = document.querySelector('#slide3');
+/*----------------- DOT-------------------*/
+let hideActiveSlide = () => {
+    let activeElement = document.querySelector('.active')
+    activeElement.classList.remove('active'); 
+}
+let showSlide = (slideNumber) => {
+    hideActiveSlide();
+    document.querySelector('#slide'+slideNumber).classList.add('active');
+}
+/*-----------ARROW------------------*/
+let showNextSlide = () => {
+    activeSlideNumber = activeSlideNumber + 1;
+    showSlide(activeSlideNumber);
+}
+/*-----------ARROW------------------*/
 
-dot2.addEventListener('click'); /* Do elementu dot2 dodaj nasłuchiwanie wydarzenia "click" */
+let showSlide1 = () => {
+    showSlide(1);
+};
+dot1.addEventListener('click', showSlide1);
+
+let showSlide2 = () => {
+    showSlide(2);
+};
+dot2.addEventListener('click', showSlide2); 
+
+
+let showSlide3 = () => {
+    showSlide(3);
+};
+
+dot3.addEventListener('click', showSlide3);
+
+/*-----------ARROW------------------*/
+arrowRight.addEventListener('click', showNextSlide);
